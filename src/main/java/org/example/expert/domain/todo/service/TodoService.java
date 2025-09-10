46714e11ca294d6da969dead27ad116763de9aac
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-10 15:25:23 +0900

    feat/lv1: 코드 개선 퀴즈 - @Transactional의 이해

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -21,10 +21,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TodoService {
-
+    
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
