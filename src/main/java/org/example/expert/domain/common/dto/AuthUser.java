cb8ac19e3af0c3d6fe209aa5a9b6debcacda6034
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-17 14:28:53 +0900

    fix/lv2: role, authUser 수정

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -1,9 +1,16 @@
 package org.example.expert.domain.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class AuthUser {
 
     private final Long id;
@@ -11,10 +18,7 @@ public class AuthUser {
     private final String nickname;
     private final UserRole userRole;
 
-    public AuthUser(Long id, String email, String nickname, UserRole userRole) {
-        this.id = id;
-        this.email = email;
-        this.nickname = nickname;
-        this.userRole = userRole;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
     }
 }
