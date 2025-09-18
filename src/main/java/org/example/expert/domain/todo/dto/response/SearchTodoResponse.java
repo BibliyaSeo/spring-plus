676413ff30d145f266f061a1da64000f0afb22d4
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-18 14:01:54 +0900

    feat/lv3: DTO에 @QueryProjection 추가

--- src/main/java/org/example/expert/domain/todo/dto/response/SearchTodoResponse.java
+++ src/main/java/org/example/expert/domain/todo/dto/response/SearchTodoResponse.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.todo.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ public class SearchTodoResponse {
     private Long managerCount;
     private Long commentCount;
 
+    @QueryProjection
     public SearchTodoResponse(String title, Long managerCount, Long commentCount) {
         this.title = title;
         this.managerCount = managerCount;
