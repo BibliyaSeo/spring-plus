da4501968fad36c1b56bd37c23e943c73bc8052e
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-18 14:51:46 +0900

    feat/lv3: log 남기기

---
+++ src/main/java/org/example/expert/domain/log/service/LogService.java
@@ -0,0 +1,22 @@
+package org.example.expert.domain.log.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.log.entity.Log;
+import org.example.expert.domain.log.repository.LogRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+    private final LogRepository logRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void recordLog(String requestDetails, String status) {
+        Log log = new Log(requestDetails, status, LocalDateTime.now());
+        logRepository.save(log);
+    }
+}
